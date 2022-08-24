48ad3c31419ae1b712677f4561c8ed88f094bea8
jsoncode <github@github.com>
2022-08-24 16:57:15 +0800

    feat: 删除无用文件

--- script/test.js
+++
@@ -1,9 +0,0 @@
-var Linter = require("eslint").Linter;
-var linter = new Linter();
-
-var messages = linter.verify("var foo", {
-    rules: {
-        semi: 2
-    }
-}, { filename: "foo.js" });
-console.log(messages)
