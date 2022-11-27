4ce884e046c6e510a6a711773b3acbf19e2b899e
jsoncode <jsoncode@qq.com>
2022-11-27 16:57:06 +0800

    feat: 添加编译后的zip包

--- zipPackl.mjs
+++ zipPackl.mjs
@@ -1,7 +1,11 @@
 import AdmZip from 'adm-zip'
 import path from 'path'
+import fs from 'fs'
 
 console.log('正在打包')
+if (fs.existsSync('./build.zip')) {
+    fs.unlinkSync('./build.zip')
+}
 // 在内存中创建新的zip文件
 const zip = new AdmZip();
 // 为zip添加本地文件夹
